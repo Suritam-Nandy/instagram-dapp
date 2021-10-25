e395d04d3a458d7c5bccc6b332dc889582e5ca3a
Suritam-Nandy <13suritsuritam@gmail.com>
2021-10-25 20:58:46 +0530

    🐛

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
 import Identicon from 'identicon.js';
-import photo from '../photo.png'
+import photo from '../photo.jpeg'
 
 class Navbar extends Component {
 
   render() {
     return (
-      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+      <nav className="navbar navbar-dark fixed-top flex-md-nowrap p-0 shadow" style={{'background-color': '#000000'}}>
         <a
-          className="navbar-brand col-sm-3 col-md-2 mr-0"
-          href="http://www.dappuniversity.com/bootcamp"
+          className="navbar-brand col-sm-8 col-md-2 mr-0"
+          href="#"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <img src={photo} width="30" height="30" className="d-inline-block align-top" alt="" />
-          Decentragram
+        <img src={photo} width="400" height="80" className="d-inline-block align-top" alt="" />
         </a>
         <ul className="navbar-nav px-3">
           <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
             <small className="text-secondary">
-              <small id="account">{'0x0'}</small>
+              <small id="account">{this.props.account}</small>
             </small>
             { this.props.account
               ? <img
